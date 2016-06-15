596fa9e0b8bdcdb1b7d5a8fa47108b68c3a7d5ec
Joe Fiorini <joe@joefiorini.com>
2016-06-15 16:13:16 -0400

    Add intl polyfill to demo

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,20 @@
-import App from './App';
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<App/>, document.getElementById('root'));
+function runMyApp() {
+  const App = require('./App');
+  render(<App/>, document.getElementById('root'));
+}
+
+if (!global.Intl) {
+  require.ensure([
+    'intl',
+    'intl/locale-data/jsonp/en.js',
+  ], function requireIntl(require) {
+    require('intl');
+    require('intl/locale-data/jsonp/en.js');
+    runMyApp();
+  });
+} else {
+  runMyApp();
+}
